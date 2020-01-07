ea057602e94473ea51fc60dbfe9562a82a9e0e98
Jooojoooo <zhaoyinjie1995@gmail.com>
2020-01-07 18:33:25 +0100

    change agency type and add a function to define target type

--- src/main/java/com/arkea/oac/model/Target.java
+++ src/main/java/com/arkea/oac/model/Target.java
@@ -5,53 +5,59 @@ import java.util.List;
 
 public class Target {
 	
-	private String target_type;//permet de controller le type de cible
-	private boolean federation;
-	private String agency;
+	private boolean federation = true;
+	private boolean agency = false;
 	private List<String> client_list = new ArrayList<>();
+	private String target_type;//permet de controller le type de cible
 	
 	public Target() {
-		super();
 	}
 	
 	
-	//Posibilité de générer des champs null, s'ils ne correspondent pas au target_type
-	public Target(String target_type, boolean federation, String agency, List<String> client_list) {
-		super();
-		this.target_type = target_type;
+	//Posibilitï¿½ de gï¿½nï¿½rer des champs null, s'ils ne correspondent pas au target_type
+	public Target(boolean federation, boolean agency, List<String> client_list) {
 		this.federation = federation;
 		this.agency = agency;
 		this.client_list = client_list;
+		this.target_type = defineTargetType(federation, agency, client_list);
 	}
 
 
-
 	public boolean isFederation() {
-		return federation;
+		return this.federation;
 	}
 	public void setFederation(boolean federation) {
 		this.federation = federation;
 	}
-	public String getAgency() {
+	public boolean getAgency() {
 		return agency;
 	}
-	public void setAgency(String agency) {
+	public void setAgency(boolean agency) {
 		this.agency = agency;
 	}
 	public List<String> getClient_list() {
-		return client_list;
+		return this.client_list;
 	}
 	public void setClient_list(List<String> client_list) {
 		this.client_list = client_list;
 	}
 
 	public String getTarget_type() {
-		return target_type;
+		return this.target_type;
 	}
 
 	public void setTarget_type(String target_type) {
 		this.target_type = target_type;
 	}
+	
 
+	public String defineTargetType (boolean federation, boolean agency, List<String> client_list) {
+		if (federation)
+			return "federation";
+		else if (agency)
+			return "agency";
+		else 
+			return "clients";
+	}
 	
 }
